a667cde1d5f894521c16e3f11bd92153ed01020d
jianghao <jianghao@haplox.com>
2021-07-21 12:46:13 +0800

    vite-react

---
+++ src/containers/GlobalFooter/index.tsx
@@ -0,0 +1,18 @@
+import { DefaultFooter } from '@ant-design/pro-layout';
+import { GithubOutlined } from '@ant-design/icons';
+
+export default function GlobalFooter() {
+  return (
+    <DefaultFooter
+      copyright={`${new Date().getFullYear()}`}
+      links={[
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/theprimone/vite-react',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+}
